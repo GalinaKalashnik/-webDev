2355cecc2880b6588a0bf74a789e2feeee669489
galaKalashnik <galina.kalashnik@balanceinternet.com.au>
2020-12-09 19:53:34 +0200

    21 redux to do list part4-local storage

--- web-dev/src/21_Redux_To_Do/reducers/tasks.js
+++ web-dev/src/21_Redux_To_Do/reducers/tasks.js
@@ -1,24 +1,18 @@
 import { ADD_TASK, REMOVE_TASK, COMPLETE_TASK } from '../../constants';
+import { load } from 'redux-localstorage-simple';
 
-const TASKS = [
-    {
-        id: 1,
-        text: 'Learn ReactJS',
-        isCompleted: true,
-    },
-    {
-        id: 2,
-        text: 'Learn Redux',
-        isCompleted: false,
-    },
-    {
-        id: 3,
-        text: 'Learn React Router',
-        isCompleted: false,
-    }
-];
+//достаем таски из локал сторадж
+let TASKS = load({ namespace: 'todo-list' });
 
-const tasks = (state = TASKS, { id, text, isCompleted, type }) => {
+console.log(TASKS);
+
+if (!TASKS || !TASKS.tasks || !TASKS.tasks.length) {
+  TASKS = {
+    tasks: [],
+  }
+}
+
+const tasks = (state = TASKS.tasks, { id, text, isCompleted, type }) => {
     switch (type) {
         case ADD_TASK :
             return [
